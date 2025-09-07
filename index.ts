5b929454ec0a6dc4b5e7e871ed326a024c6b6cb4
Antikore <koopatropa8@hotmail.com>
2025-09-07 13:33:24 +0200

    🚨 Add unused websocket to calm down the linter

--- index.ts
+++ index.ts
@@ -20,6 +20,19 @@ const PORT = config.host.port || 3621;
 Bun.serve({
 	port: PORT,
 	routes: routes,
+
+	// Added to calm down the wrath of the IDE
+	websocket: {
+		open(ws) {
+			console.log('Connected');
+		},
+		close(ws){
+			console.log('Disconnected');
+		},
+		message(ws, message){
+			console.log('Message');
+		}
+	},
 	fetch(req){
 		return new Response("Method Not Allowed", { status: 405, headers: { 'Access-Control-Allow-Origin': '*' } });
 	}
