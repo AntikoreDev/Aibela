02fc381fc4b33ba5d1c6f18e239f22f33449275c
Antikore <koopatropa8@hotmail.com>
2025-09-22 19:05:58 +0200

    💡 Add schema comments

--- database/schema.ts
+++ database/schema.ts
@@ -12,15 +12,15 @@ export const channels = sqliteTable("channels", {
 });
 
 export const videos = sqliteTable("videos", {
-	id: integer({ mode: "number" }).primaryKey({ autoIncrement:true }).unique(),
-	name: text().notNull(),
-	title: text().notNull(),
-	description: text(),
-	upload_date: text().default(sql`(CURRENT_TIMESTAMP)`),
-	language: text().default(''),
-	prev: integer({ mode: "number" }),
-	next: integer({ mode: "number" }),
-	visible: integer({ mode: "boolean" }).default(true),
-	access_key: text(),
-	channel: integer({ mode: "number" }).references(() => channels.id).notNull(),
+	id: integer({ mode: "number" }).primaryKey({ autoIncrement:true }).unique(), // Internal identifier
+	name: text().notNull(), // Unique identifier to ease access
+	title: text().notNull(), // Video title
+	description: text(), // Video description
+	upload_date: text().default(sql`(CURRENT_TIMESTAMP)`), // Upload date. Can be changed but in general it shouldn't
+	language: text().default(''), // Video language. Defaults to no language.
+	prev: integer({ mode: "number" }), // Previous video ID in case this is on a series
+	next: integer({ mode: "number" }), // Next video ID in case this is on a series.
+	visible: integer({ mode: "boolean" }).default(true), // If the video is visible as is.
+	access_key: text(), // Access key, used to access the video in case it is not visible
+	channel: integer({ mode: "number" }).references(() => channels.id).notNull(), // The channel considered the video owner
 });
